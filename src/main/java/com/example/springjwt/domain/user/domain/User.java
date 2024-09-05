420a8d4e3af7982ff21c570ffd20b9c7080e0e55
도훈 <modooboiroo@gmail.com>
2024-09-05 23:04:35 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/example/springjwt/domain/user/domain/User.java
@@ -0,0 +1,26 @@
+package com.example.springjwt.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String password;
+
+}
