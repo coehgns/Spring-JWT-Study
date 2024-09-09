7e7c59d6034462d55910c8fbc0edb033af850609
도훈 <modooboiroo@gmail.com>
2024-09-09 19:12:47 +0900

    feat: BoardResponse 추가

---
+++ src/main/java/com/example/springjwt/domain/board/presentation/response/BoardResponse.java
@@ -0,0 +1,24 @@
+package com.example.springjwt.domain.board.presentation.response;
+
+import com.example.springjwt.domain.board.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BoardResponse {
+
+    private String username;
+
+    private String title;
+
+    private String content;
+
+    public BoardResponse(Board board) {
+        username = board.getUser().getUsername();
+        title = board.getTitle();
+        content = board.getContent();
+    }
+}
