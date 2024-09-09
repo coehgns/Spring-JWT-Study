66718cdb6584788c63c0a0df8c0ba8ad3d89ff42
도훈 <modooboiroo@gmail.com>
2024-09-09 19:35:40 +0900

    feat: ErrorCode에 BoardAuthorMismatch 추가

--- src/main/java/com/example/springjwt/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/springjwt/global/error/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(404, "User not found."),
 
-    BOARD_NOT_FOUND(404, "Board not found.");
+    BOARD_NOT_FOUND(404, "Board not found."),
+
+    BOARD_AUTHOR_MISMATCH(403, "Board author mismatch");
 
     private final int statusCode;
     private final String message;
