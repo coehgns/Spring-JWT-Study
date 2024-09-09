ec6dc6d3ae34e59707f5a8cc89c3f36408e55d9d
도훈 <modooboiroo@gmail.com>
2024-09-09 19:13:16 +0900

    feat: SelectBoardService 추가

---
+++ src/main/java/com/example/springjwt/domain/board/service/SelectBoardService.java
@@ -0,0 +1,22 @@
+package com.example.springjwt.domain.board.service;
+
+import com.example.springjwt.domain.board.domain.Board;
+import com.example.springjwt.domain.board.domain.repository.BoardRepository;
+import com.example.springjwt.domain.board.exception.BoardNotFoundException;
+import com.example.springjwt.domain.board.presentation.response.BoardResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SelectBoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardResponse execute(Long boardId) {
+        Board board  = boardRepository.findById(boardId)
+                .orElseThrow(() -> BoardNotFoundException.EXCEPTION);
+
+        return new BoardResponse(board);
+    }
+}
