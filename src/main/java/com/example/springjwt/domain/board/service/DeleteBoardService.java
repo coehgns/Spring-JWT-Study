534f1ad0c5acb6eafe7efe42f7f832b4c4c4aa74
도훈 <modooboiroo@gmail.com>
2024-09-09 19:44:59 +0900

    feat: DeleteBoardService 추가

---
+++ src/main/java/com/example/springjwt/domain/board/service/DeleteBoardService.java
@@ -0,0 +1,30 @@
+package com.example.springjwt.domain.board.service;
+
+import com.example.springjwt.domain.board.domain.Board;
+import com.example.springjwt.domain.board.domain.repository.BoardRepository;
+import com.example.springjwt.domain.board.exception.BoardNotFoundException;
+import com.example.springjwt.domain.board.facade.BoardFacade;
+import com.example.springjwt.domain.user.domain.User;
+import com.example.springjwt.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteBoardService {
+
+    private final BoardRepository boardRepository;
+    private final UserFacade userFacade;
+    private final BoardFacade boardFacade;
+
+    public void execute(Long boardId) {
+        User currentUser = userFacade.currentUser();
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> BoardNotFoundException.EXCEPTION);
+
+        boardFacade.authorCheck(currentUser, board);
+
+        boardRepository.deleteById(boardId);
+    }
+}
