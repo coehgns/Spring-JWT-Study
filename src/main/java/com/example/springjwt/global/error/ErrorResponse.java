6ad9508939f83e2434083190235064653f75f1f0
도훈 <modooboiroo@gmail.com>
2024-09-10 17:35:39 +0900

    feat: ErrorResponse 추가

---
+++ src/main/java/com/example/springjwt/global/error/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.example.springjwt.global.error;
+
+import com.example.springjwt.global.error.exception.ErrorCode;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+    private Integer status;
+    private String description;
+
+    public static ErrorResponse of(ErrorCode errorCode, String description) {
+        return ErrorResponse.builder()
+                .message(errorCode.getMessage())
+                .status(errorCode.getStatusCode())
+                .description(description)
+                .build();
+    }
+
+}
