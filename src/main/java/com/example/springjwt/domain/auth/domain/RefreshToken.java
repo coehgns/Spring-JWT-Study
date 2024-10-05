725e713b573c2da3202ffb86ae00ca94b83d1692
도훈 <modooboiroo@gmail.com>
2024-10-05 23:40:41 +0900

    feat: RefreshToken 추가

---
+++ src/main/java/com/example/springjwt/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,23 @@
+package com.example.springjwt.domain.auth.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "token")
+    private String token;
+}
