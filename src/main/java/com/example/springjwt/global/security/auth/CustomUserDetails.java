e752345fc577ba91137b57af429221699070c78b
도훈 <modooboiroo@gmail.com>
2024-10-06 00:38:42 +0900

    feat: CustomUserDetails 추가

---
+++ src/main/java/com/example/springjwt/global/security/auth/CustomUserDetails.java
@@ -0,0 +1,47 @@
+package com.example.springjwt.global.security.auth;
+
+import com.example.springjwt.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public record CustomUserDetails(User user) implements UserDetails {
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority(user.getEmail())));
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
