7cc89e3df1c540d83f3dbad569665f33f8851f21
도훈 <modooboiroo@gmail.com>
2024-10-06 21:40:54 +0900

    chore: 반환값 설정

--- src/main/java/com/example/springjwt/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/AuthController.java
@@ -24,6 +24,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public TokenResponse login(@RequestBody LoginRequest request) {
-        authService.login(request);
+        return authService.login(request);
     }
 }
