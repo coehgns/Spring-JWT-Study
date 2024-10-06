d3ed9c7d2705558d3594003a3803b5c885ba9bba
도훈 <modooboiroo@gmail.com>
2024-10-06 21:12:21 +0900

    feat: Builder 추가

--- src/main/java/com/example/springjwt/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/dto/response/TokenResponse.java
@@ -1,8 +1,10 @@
 package com.example.springjwt.domain.auth.presentation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
