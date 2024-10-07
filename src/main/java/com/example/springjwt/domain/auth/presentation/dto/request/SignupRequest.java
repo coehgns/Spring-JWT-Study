e5606f44c8b44473d147b7f6b8b6e81c180e9db6
도훈 <modooboiroo@gmail.com>
2024-10-08 08:38:12 +0900

    feat: @NotBlank 추가

--- src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/SignupRequest.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/SignupRequest.java
@@ -1,11 +1,12 @@
 package com.example.springjwt.domain.auth.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
-
+    @NotBlank
     private String username;
-
+    @NotBlank
     private String password;
 }
