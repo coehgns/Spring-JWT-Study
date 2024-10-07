bcf39e932b9504c54511d7cc9ff115f8835f2879
도훈 <modooboiroo@gmail.com>
2024-10-07 13:55:36 +0900

    feat: JwtTokenProvider 추가

---
+++ src/main/java/com/example/springjwt/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,77 @@
+package com.example.springjwt.global.security.jwt;
+
+import com.example.springjwt.domain.auth.domain.RefreshToken;
+import com.example.springjwt.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.springjwt.domain.auth.exception.ExpiredTokenException;
+import com.example.springjwt.domain.auth.exception.InvalidTokenException;
+import com.example.springjwt.domain.user.domain.repository.UserRepository;
+import com.example.springjwt.global.security.auth.CustomUserDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final JwtProperties jwtProperties;
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public String createAccessToken(String accountId) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(accountId)
+                .claim("type", "access")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getAccessExpiration() * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    public String createRefreshToken(String accountId) {
+        Date now = new Date();
+
+        String rfToken = Jwts.builder()
+                .setSubject(accountId)
+                .claim("type", "refresh")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getRefreshExpiration() * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .username(accountId)
+                        .token(rfToken)
+                        .build());
+
+        return rfToken;
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(jwtProperties.getSecretKey()).build().parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+}
