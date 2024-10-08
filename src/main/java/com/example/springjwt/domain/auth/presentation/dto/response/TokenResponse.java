1f96f1aa666bc76475c59fc4cbb027d44463c462
도훈 <modooboiroo@gmail.com>
2024-10-08 19:19:14 +0900

    feat: accessExpiredAt, refreshExpiredAt 추가

--- src/main/java/com/example/springjwt/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/dto/response/TokenResponse.java
@@ -3,9 +3,13 @@ package com.example.springjwt.domain.auth.presentation.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
 @Builder
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
+    private Date accessExpiredAt;
+    private Date refreshExpiredAt;
 }
