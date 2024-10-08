c6ebfba703f127b57d5000155c83b4a4feee43ae
도훈 <modooboiroo@gmail.com>
2024-10-08 22:04:33 +0900

    feat: existsByUsername 추가

--- src/main/java/com/example/springjwt/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/springjwt/domain/user/domain/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    Optional<User> existsByUsername(String username);
+    Boolean existsByUsername(String username);
 }
