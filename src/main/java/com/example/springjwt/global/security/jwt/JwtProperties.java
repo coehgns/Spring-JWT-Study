1c4fbadd4d6032b693c61552ca368b897cdf792f
도훈 <modooboiroo@gmail.com>
2024-10-08 18:49:22 +0900

    feat: @Setter 추가

--- src/main/java/com/example/springjwt/global/security/jwt/JwtProperties.java
+++ src/main/java/com/example/springjwt/global/security/jwt/JwtProperties.java
@@ -1,9 +1,11 @@
 package com.example.springjwt.global.security.jwt;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Setter
 @Getter
 @Component
 @ConfigurationProperties(prefix = "jwt")
