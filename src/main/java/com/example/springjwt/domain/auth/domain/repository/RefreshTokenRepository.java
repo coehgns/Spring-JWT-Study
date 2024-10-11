8d5ca82f5984728bd08e3b09d0ca9bdc9cf89d00
도훈 <modooboiroo@gmail.com>
2024-10-11 16:34:52 +0900

    feat: @Repository 추가

--- src/main/java/com/example/springjwt/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/springjwt/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -2,9 +2,11 @@ package com.example.springjwt.domain.auth.domain.repository;
 
 import com.example.springjwt.domain.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
 }
