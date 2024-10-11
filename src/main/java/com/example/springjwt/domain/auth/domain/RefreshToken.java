7cf65cb9060a427eadf0b2da8123c75585f8299b
도훈 <modooboiroo@gmail.com>
2024-10-11 21:42:31 +0900

    refactor: @Id import 수정

--- src/main/java/com/example/springjwt/domain/auth/domain/RefreshToken.java
+++ src/main/java/com/example/springjwt/domain/auth/domain/RefreshToken.java
@@ -1,7 +1,8 @@
 package com.example.springjwt.domain.auth.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
