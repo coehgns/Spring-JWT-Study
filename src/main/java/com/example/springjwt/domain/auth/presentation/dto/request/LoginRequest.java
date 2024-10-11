937abfa16f1f6209580f30cb2b4b07956af9a13b
도훈 <modooboiroo@gmail.com>
2024-10-11 16:53:47 +0900

    feat: @NoArgsConstructor 추가

--- src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.example.springjwt.domain.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequest {
     @NotBlank
     private String username;
--- src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/ReIssueRequest.java
+++ src/main/java/com/example/springjwt/domain/auth/presentation/dto/request/ReIssueRequest.java
@@ -1,8 +1,11 @@
 package com.example.springjwt.domain.auth.presentation.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReIssueRequest {
     private String refreshToken;
 }
