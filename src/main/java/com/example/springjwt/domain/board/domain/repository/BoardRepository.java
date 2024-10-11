e304ddba014e4356b21e992340bf3666c8b0331e
도훈 <modooboiroo@gmail.com>
2024-10-11 17:13:52 +0900

    feat: @repository 추가

--- src/main/java/com/example/springjwt/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/springjwt/domain/board/domain/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.springjwt.domain.board.domain.repository;
 
 import com.example.springjwt.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
