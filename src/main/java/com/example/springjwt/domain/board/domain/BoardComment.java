2d603ce12b87e252f2d7e2c7bf5c3b68d9b2d686
도훈 <modooboiroo@gmail.com>
2024-10-13 22:44:01 +0900

    feat: BoardComment 추가

---
+++ src/main/java/com/example/springjwt/domain/board/domain/BoardComment.java
@@ -0,0 +1,29 @@
+package com.example.springjwt.domain.board.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(name = "writer")
+    private String writer;
+}
