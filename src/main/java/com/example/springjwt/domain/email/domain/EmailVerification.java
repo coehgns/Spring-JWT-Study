ab9608f1404e234fa252e9b5171d9784b64101ac
도훈 <modooboiroo@gmail.com>
2024-10-23 17:11:13 +0900

    feat: EmailVerification 추가

---
+++ src/main/java/com/example/springjwt/domain/email/domain/EmailVerification.java
@@ -0,0 +1,31 @@
+package com.example.springjwt.domain.email.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String authCode;
+
+    @Column(nullable = false)
+    private Boolean isVerified;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
