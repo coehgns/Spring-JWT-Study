d16e745a8bd198e50602b603c7153d0f0233d793
도훈 <modooboiroo@gmail.com>
2024-10-26 23:37:40 +0900

    feat: findByEmail 추가

--- src/main/java/com/example/springjwt/domain/email/domain/repository/EmailVerificationRepository.java
+++ src/main/java/com/example/springjwt/domain/email/domain/repository/EmailVerificationRepository.java
@@ -4,6 +4,9 @@ import com.example.springjwt.domain.email.domain.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+    Optional<EmailVerification> findByEmail(String email);
 }
