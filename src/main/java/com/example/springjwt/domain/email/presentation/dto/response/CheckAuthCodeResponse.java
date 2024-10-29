a041e250a545952a7f2a8056d739c4aa23322421
도훈 <modooboiroo@gmail.com>
2024-10-29 22:40:12 +0900

    feat: @Builder 추가

--- src/main/java/com/example/springjwt/domain/email/presentation/dto/response/CheckAuthCodeResponse.java
+++ src/main/java/com/example/springjwt/domain/email/presentation/dto/response/CheckAuthCodeResponse.java
@@ -1,8 +1,10 @@
 package com.example.springjwt.domain.email.presentation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CheckAuthCodeResponse {
     private boolean isVerified;
 }
