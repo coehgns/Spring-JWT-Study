e40486c47db1fd3d758fbe679814605c2b72f662
도훈 <modooboiroo@gmail.com>
2024-10-30 21:49:14 +0900

    feat: SendAuthCodeService

---
+++ src/main/java/com/example/springjwt/domain/email/service/SendAuthCodeService.java
@@ -0,0 +1,39 @@
+package com.example.springjwt.domain.email.service;
+
+import com.example.springjwt.domain.email.domain.EmailVerification;
+import com.example.springjwt.domain.email.domain.repository.EmailVerificationRepository;
+import com.example.springjwt.domain.email.exception.EmailNotVerifiedException;
+import com.example.springjwt.domain.email.facade.EmailVerificationFacade;
+import com.example.springjwt.domain.email.presentation.dto.request.SendEmailRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class SendAuthCodeService {
+
+    private final EmailService emailService;
+    private final EmailVerificationFacade emailVerificationFacade;
+    private final EmailVerificationRepository emailVerificationRepository;
+
+    private void sendAuthCode(SendEmailRequest request) {
+        String email = request.getEmail();
+        String authCode = makeAuthCode();
+
+        EmailVerification emailVerification = emailVerificationFacade.getEmailVerificationByEmail(request.getEmail());
+        emailVerificationRepository.save(new EmailVerification(email, authCode));
+
+        emailService.sendEmail(email, authCode);
+    }
+
+    private String makeAuthCode() {
+        Random random = new Random();
+        String code = "";
+        for (int i = 0; i < 6; i++) {
+            code += (int) (random.nextDouble() * 10);
+        }
+        return code;
+    }
+}
