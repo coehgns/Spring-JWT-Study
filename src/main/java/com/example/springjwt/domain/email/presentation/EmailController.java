a3dac1980149f3bbea548a4e186647dfbbd5e93f
도훈 <modooboiroo@gmail.com>
2024-10-30 21:58:39 +0900

    feat: EmailController

---
+++ src/main/java/com/example/springjwt/domain/email/presentation/EmailController.java
@@ -0,0 +1,31 @@
+package com.example.springjwt.domain.email.presentation;
+
+import com.example.springjwt.domain.email.presentation.dto.request.CheckAuthCodeRequest;
+import com.example.springjwt.domain.email.presentation.dto.request.SendEmailRequest;
+import com.example.springjwt.domain.email.presentation.dto.response.CheckAuthCodeResponse;
+import com.example.springjwt.domain.email.service.CheckAuthCodeService;
+import com.example.springjwt.domain.email.service.SendAuthCodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final CheckAuthCodeService checkAuthCodeService;
+    private final SendAuthCodeService sendAuthCodeService;
+
+    @PostMapping("send-authCode")
+    public void sendAuthCode(@RequestBody SendEmailRequest request) {
+        sendAuthCodeService.sendAuthCode(request);
+    }
+
+    @PostMapping("/check-authCode")
+    public CheckAuthCodeResponse checkAuthCode(@RequestBody CheckAuthCodeRequest request) {
+        return checkAuthCodeService.checkAuthCode(request);
+    }
+}
