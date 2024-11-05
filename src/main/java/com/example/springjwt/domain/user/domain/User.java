2dcc2f8d297412bce1a05150ac9189fddd9ef827
도훈 <modooboiroo@gmail.com>
2024-11-05 21:24:11 +0900

    feat :: 연관관계 설정

--- src/main/java/com/example/springjwt/domain/user/domain/User.java
+++ src/main/java/com/example/springjwt/domain/user/domain/User.java
@@ -1,7 +1,11 @@
 package com.example.springjwt.domain.user.domain;
 
+import com.example.springjwt.domain.board.domain.Board;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,4 +25,6 @@ public class User {
     @Column(unique = true)
     private String password;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Board> boardList = new ArrayList<>();
 }
