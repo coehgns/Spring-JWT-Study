bc9f891c93b09afb1e4db658f9d0b0ae3ecdc78c
도훈 <modooboiroo@gmail.com>
2024-11-11 17:09:27 +0900

    refactor :: 폴더 구조 재개편

--- src/main/java/com/example/springjwt/global/config/mail/MailConfig.java
+++ src/main/java/com/example/springjwt/global/config/mail/MailConfig.java
@@ -1,4 +1,4 @@
-package com.example.springjwt.global.config;
+package com.example.springjwt.global.config.mail;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
