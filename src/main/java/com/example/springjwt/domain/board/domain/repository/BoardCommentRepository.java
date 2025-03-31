a4c23d79d1e7753384c78d50fb933795b583c19d
도훈 <modooboiroo@gmail.com>
2025-03-31 19:58:42 +0900

    chore : 모든 파일 삭제

--- src/main/java/com/example/springjwt/domain/board/domain/repository/BoardCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springjwt.domain.board.domain.repository;
-
-import com.example.springjwt.domain.board.domain.BoardComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
-}
--- src/main/java/com/example/springjwt/domain/email/presentation/dto/request/CheckAuthCodeRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springjwt.domain.email.presentation.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class CheckAuthCodeRequest {
-    private String email;
-    private String authCode;
-}
